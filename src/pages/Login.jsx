4b9a2a4ae61ba6b21a72d6301b783832713403bf
Nelson Feng <nelsonfeng98@gmail.com>
2022-09-06 18:22:15 +1000

    Css update login, sign up and home

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     return (
         <div className='formContainer'>
             <div className="formWrapper">
-                <span className="logo">Letr</span>
+                <span className="logo" >Letr</span>
                 <span className="title">Login</span>
                 <form onSubmit={handleSubmit}>
                     <input type="email" placeholder="Email"/>
