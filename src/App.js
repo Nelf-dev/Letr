544cd72bd222218f2ae8cf788531e5c9f99c9aed
Nelson Feng <nelsonfeng98@gmail.com>
2022-09-06 22:52:21 +1000

    added light mode and dark mode

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 import "./style.scss";
-import { BrowserRouter, Routes, Route, Navigate, } from "react-router-dom";
-import { useContext } from 'react';
-import { AuthContext } from './context/AuthContext';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext, createContext, useState } from "react";
+import { AuthContext } from "./context/AuthContext";
+import ReactSwitch from "react-switch";
 
+//Light and Dark mode Context
+
+export const ThemeContext = createContext(null);
 
 function App() {
 
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
 
-  const ProtectedRoute = ({children}) =>{
-    if(!currentUser){
-      return <Navigate to="/login" />
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
 
-    return children
+    return children;
   };
-  
+
+  //Light and Dark Mode
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr == "light" ? "dark" : "light"));
+  };
+  // *******************
+
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            } />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <div className="switch">
+          <ReactSwitch 
+          onChange={toggleTheme} 
+          checked={theme === "dark"}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          onColor={"#272c36"}
+          offColor={"#9fa3ac"}
+          />
+        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        </div>
+    </ThemeContext.Provider>
   );
 }
 
