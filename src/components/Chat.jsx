bdeec03ccad88bd6f68e48fed0e491e538684772
Nelson Feng <nelsonfeng98@gmail.com>
2022-09-06 10:34:04 +1000

    SCSS - chats,search and side bar

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const Chat = () => {
     return (
         <div className="chat">
+            <div className="chatInfo">
+                <span>Jane</span>
+            </div>
         </div>
     );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,12 +4,12 @@ const Search = () => {
     return (
         <div className="search">
             <div className="searchForm">
-                <input type="text" />
+                <input type="text" placeholder="Find a friend"/>
             </div>
-            <div className="userChat">
-            <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
-                <div className="userChatInfo">
-                    <span>James</span>
+                <div className="userChat">
+                    <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
+                    <div className="userChatInfo">
+                        <span>James</span>
                 </div>
             </div>
         </div>
