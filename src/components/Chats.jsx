48c874426b07831845601bc16e9564d298ee8c55
Lukas Eichler <lukas.eichler@yahoo.com>
2022-09-06 22:30:16 +1000

    fetch chat convos done

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,46 +1,50 @@
-import React from 'react';
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+// import { ChatContext } from "../context/ChatContext";
 
 const Chats = () => {
-    return (
-        <div>
-            <div className="userChat">
-                    <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
-                    <div className="userChatInfo">
-                        <span>James</span>
-                        <p>Hello :)</p>
-                </div>
-            </div>
-            <div className="userChat">
-                    <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
-                    <div className="userChatInfo">
-                        <span>James</span>
-                        <p>Hello :)</p>
-                </div>
-            </div>
-            <div className="userChat">
-                    <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
-                    <div className="userChatInfo">
-                        <span>James</span>
-                        <p>Hello :)</p>
-                </div>
-            </div>
-            <div className="userChat">
-                    <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
-                    <div className="userChatInfo">
-                        <span>James</span>
-                        <p>Hello :)</p>
-                </div>
-            </div>
-            <div className="userChat">
-                    <img src="https://images.unsplash.com/photo-1611403119860-57c4937ef987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXNpYW4lMjBtYWxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="" />
-                    <div className="userChatInfo">
-                        <span>James</span>
-                        <p>Hello :)</p>
-                </div>
-            </div>
-            
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+//   const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const handleSelect = (u) => {
+    // dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
+  return (
+    <div className="chats">
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+        <div
+          className="userChat"
+          key={ chat[0] }
+          onClick={ () => handleSelect(chat[1].userInfo) }
+        >
+          <img src={ chat[1].userInfo.photoURL } alt="" />
+          <div className="userChatInfo">
+            <span>{ chat[1].userInfo.displayName }</span>
+            <p>{ chat[1].lastMessage?.text }</p>
+          </div>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default Chats;
