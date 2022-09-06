a58a68215cbc99219c9b77c9512f8f0b2fca1753
dkdam <dkdam@live.com>
2022-09-06 12:57:06 +1000

    setting up back-end

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import "./style.scss";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAWmhphp9gBeUdAqXrKmw8ktU2Px2cTm70",
+    authDomain: "letr-c11e5.firebaseapp.com",
+    projectId: "letr-c11e5",
+    storageBucket: "letr-c11e5.appspot.com",
+    messagingSenderId: "551991798037",
+    appId: "1:551991798037:web:ed25932c489bdf7329211c",
+    measurementId: "G-LE5CDWMJCN"
+};
+
+// const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+export const storage = getStorage();
+export const db = getFirestore()
