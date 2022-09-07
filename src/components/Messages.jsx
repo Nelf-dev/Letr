1560dc50aed83ca07bc4ee27fca2c7a0f9db31d5
dkdam <dkdam@live.com>
2022-09-07 11:32:56 +1000

    messages.jsx debug

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -23,11 +23,11 @@ const Messages = () => {
 
     return (
         <div className="messages">
-            {messages.map(m=>(
-                <Message message={m}/>
+            {messages.map((m) => (
+            <Message message={m} key={m.id} />
             ))}
         </div>
-    );
+        );
 }
 
 export default Messages;
